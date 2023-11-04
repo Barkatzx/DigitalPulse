1ccb1ee8e776d0163fc5a641660ae65218ba45bf
Barkatzx <barkatullah.zx@gmail.com>
2023-11-05 04:17:19 +0600

    Footer And Contact Form Added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
- // JavaScript function to toggle the mobile menu
- function toggleMobileMenu() {
+// JavaScript function to toggle the mobile menu
+function toggleMobileMenu() {
   var mobileMenuContent = document.getElementById('mobile-menu-content');
   if (mobileMenuContent.style.display === 'none' || mobileMenuContent.style.display === '') {
     mobileMenuContent.style.display = 'block';
@@ -7,3 +7,24 @@
     mobileMenuContent.style.display = 'none';
   }
 }
+
+// For Smooth Scrooling
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll("nav a");
+
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      const targetId = this.getAttribute("href").substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      if (targetSection) {
+        window.scrollTo({
+          top: targetSection.offsetTop,
+          behavior: "smooth",
+        });
+      }
+    });
+  });
+});
